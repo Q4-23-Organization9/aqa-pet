589a6e4d0138c1c264c9e55145743cb44cbf6d64
Olha <olha.chernikova.qa@gmail.com>
2023-12-19 21:25:02 +0200

    add 3 files

---
+++ package/Task_4_2.java
@@ -0,0 +1,9 @@
+public class Task_4_2 {
+    public static void main(String[] args) {
+        double cel = 36.6;
+        double fahr = (cel * 9 / 5) + 32;
+        double kel = cel + 273.16;
+        System.out.println(cel+" Celsius: "+ fahr + " Fahrenheit;   "+ cel+" Celsius: "+ kel+ " Kelvin;");
+
+    }
+}
---
+++ package/Task_4_3.java
@@ -0,0 +1,9 @@
+public class Task_4_3 {
+    public static void main(String[] args) {
+        int side1 = 7;
+        int side2 = 5;
+        double square = (double) ( side1 * side2 ) / 2;
+
+        System.out.println( "square = "+square );
+    }
+}
