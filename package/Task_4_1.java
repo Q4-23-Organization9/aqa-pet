b40bff897b0f36e6d54d2ee8607ada324224128f
Olha <olha.chernikova.qa@gmail.com>
2023-12-21 11:20:45 +0200

    edit 3 files

--- package/Task_4_1.java
+++ package/Task_4_1.java
@@ -1,5 +1,8 @@
 public class Task_4_1 {
     public static void main(String[] args) {
+
         System.out.println("Hello, Java world! Welcome to the adventure!");
+        System.out.println("Hello, World!");
+
     }
 }
