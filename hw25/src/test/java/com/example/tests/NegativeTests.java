6faf0b1dec561af1fc1bcc0a665d2abab3e25629
Olha <olha.chernikova.qa@gmail.com>
2024-04-21 16:02:53 +0300

    home work 25

---
+++ hw25/src/test/java/com/example/tests/NegativeTests.java
@@ -0,0 +1,89 @@
+package com.example.tests;
+
+import com.example.model.User;
+import io.qameta.allure.Severity;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import static io.qameta.allure.SeverityLevel.MINOR;
+import static io.restassured.RestAssured.given;
+
+public class NegativeTests {
+
+    @BeforeSuite
+    public void setUpSuite() {
+
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
+                .setBaseUri("https://reqres.in")
+                .build();
+    }
+    @Severity(MINOR)
+    @Test(dataProvider = "wrongEndpointAndUserData", dataProviderClass = DataProviders.class)
+    public void negativeTestCreateUser(String wrongEndpoint, String name, String job) {
+        User user = new User(name, job);
+
+        given()
+
+                .contentType(ContentType.JSON)
+                .body(user.toJson())
+                .when()
+                .post(wrongEndpoint)
+                .then()
+
+                .assertThat()
+                .statusCode(404);
+    }
+
+    @Severity(MINOR)
+    @Test(dataProvider = "nonExistingUserId", dataProviderClass = DataProviders.class)
+    public void testGetNonExistingUser(int userId) {
+        given()
+                .pathParam("userId", userId)
+                .when()
+                .get("/api/users/{userId}")
+                .then()
+                .assertThat()
+                .statusCode(404);
+    }
+
+
+
+
+    @Severity(MINOR)
+    @Test(dataProvider = "negativeFixUserData", dataProviderClass = DataProviders.class)
+    public void testUpdateNonExistingUser(int userId, String updatedName, String updatedJob) {
+        User user = new User(updatedName, updatedJob);
+
+        given()
+                .pathParam("userId", userId)
+                .contentType(ContentType.JSON)
+                .body(user.toJson())
+                .when()
+                .put("/api/users/{userId}")
+                .then()
+                .statusCode(404);
+    }
+
+
+
+
+    @Severity(MINOR)
+    @Test(dataProvider = "nonExistingUserId", dataProviderClass = DataProviders.class)
+    public void testDeleteNonExistingUser (int userId) {
+        given()
+                .when()
+                .delete("/api/users/" + userId)
+                .then()
+                .assertThat()
+                .statusCode(404);
+    }
+
+
+}
---
+++ hw25/src/test/java/com/example/tests/PositiveTests.java
@@ -0,0 +1,108 @@
+package com.example.tests;
+
+import com.example.model.User;
+import io.qameta.allure.Severity;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static io.qameta.allure.SeverityLevel.CRITICAL;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.startsWith;
+
+public class PositiveTests {
+
+    @BeforeSuite
+    public void setUpSuite() {
+
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
+                .setBaseUri("https://reqres.in")
+                .build();
+    }
+    @Severity(CRITICAL)
+    @Test(dataProvider = "userData", dataProviderClass = DataProviders.class)
+    public void testCreateUser(String name, String job) {
+        User user = new User(name, job);
+
+        given()
+
+                .contentType(ContentType.JSON)
+                .body(user.toJson())
+                .when()
+                .post("/api/users")
+                .then()
+
+                .assertThat()
+                .statusCode(201)
+                .body("name", equalTo(name))
+                .body("job", equalTo(job))
+                .body("id", notNullValue())
+                .body("createdAt", notNullValue());
+    }
+
+
+    @Severity(CRITICAL)
+    @Test(dataProvider = "positiveUserData", dataProviderClass = DataProviders.class)
+    public void testGetExistingUser(int userId) {
+        given()
+                .pathParam("userId", userId)
+                .when()
+                .get("/api/users/{userId}")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .body("data.id", equalTo(userId));
+    }
+
+
+    @Severity(CRITICAL)
+    @Test(dataProvider = "positiveFixUserData", dataProviderClass = DataProviders.class)
+    public void testUpdateUser(int userId, String updatedName, String updatedJob) {
+        User user = new User(updatedName, updatedJob);
+
+        given()
+                .pathParam("userId", userId)
+                .contentType(ContentType.JSON)
+                .body(user.toJson())
+                .when()
+                .put("/api/users/{userId}")
+                .then()
+                .statusCode(200)
+                .body("name", equalTo(updatedName))
+                .body("job", equalTo(updatedJob))
+                .body("updatedAt", startsWith(getCurrentDate()));
+    }
+    private String getCurrentDate() {
+
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return currentDate.format(formatter);
+
+    }
+
+
+
+    @Severity(CRITICAL)
+    @Test(dataProvider = "positiveUserData", dataProviderClass = DataProviders.class)
+    public void positiveDeleteUser (int userId) {
+        given()
+                .when()
+                .delete("/api/users/" + userId)
+                .then()
+                .assertThat()
+                .statusCode(204);
+    }
+
+
+}
